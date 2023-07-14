5ad27ddfe3e01b5ba383541342b2588e544a2699
benjaminluan <benjaminluan1@gmail.com>
2023-07-14 18:44:02 -0400

    Resolving github dumb issue

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/send-money">
         <Wrapper>
           <Navbar />
           <Routes>
